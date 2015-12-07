39f1bde8cf7651f2590b3a1e0f90c68b0b03c516
StoneMoe <lakechan96@gmail.com>
2015-12-08 00:24:56 +0800

    Changed some text

--- src/EasyCUSX/App.xaml.cs
+++ src/EasyCUSX/App.xaml.cs
@@ -33,12 +33,12 @@ namespace EasyCUSX
 
         static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
         {
-            MessageBox.Show("易·山传在运行过程中发生了一个严重错误，程序将尝试忽略这个错误并继续工作\r\n但仍建议您重新打开易·山传以避免一些未知的问题.\r\n希望您能将下面的错误信息报告给开发者以帮助解决这个问题，不胜感激！反馈QQ群：512985336\r\n错误堆栈：" + e.ExceptionObject.ToString());
+            MessageBox.Show("易·山传在运行过程中发生了一个严重错误，程序将尝试忽略这个错误并继续工作\r\n但仍建议您重新打开易·山传，以避免一些未知的问题.\r\n希望您能将下面的错误信息反馈给开发者以帮助解决这个问题，不胜感激！反馈QQ群：512985336\r\n错误堆栈：" + e.ExceptionObject.ToString());
         }
 
         static void application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
         {
-            MessageBox.Show("易·山传在运行过程中发生了一个严重错误，程序将尝试忽略这个错误并继续工作\r\n但仍建议您重新打开易·山传以避免一些未知的问题.\r\n希望您能将下面的错误信息报告给开发者以帮助解决这个问题，不胜感激！反馈QQ群：512985336\r\n错误堆栈：" + e.Exception.ToString());
+            MessageBox.Show("易·山传在运行过程中发生了一个严重错误，程序将尝试忽略这个错误并继续工作\r\n但仍建议您重新打开易·山传，以避免一些未知的问题.\r\n希望您能将下面的错误信息反馈给开发者以帮助解决这个问题，不胜感激！反馈QQ群：512985336\r\n错误堆栈：" + e.Exception.ToString());
             e.Handled = true;
         }
 
