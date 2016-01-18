157f37bf2a37e9a9c48309c38118a773f60e1204
StoneMoe <lakechan96@gmail.com>
2016-01-19 05:27:40 +0800

    Changed exception handler info

--- src/EasyCUSX/ExceptionHandler.cs
+++ src/EasyCUSX/ExceptionHandler.cs
@@ -10,8 +10,8 @@ namespace ExHandler
         {
             try
             {
-                File.AppendAllText(Application.StartupPath + @"\Exception.txt", string.Format("\r\n====异常信息====\r\n时间:{0}\r\n====错误堆栈====\r\n{1}\r\n====错误堆栈结束====\r\n====异常信息结束====\r\n", DateTime.Now.ToLocalTime().ToString(), exStack));
-                Console.WriteLine(string.Format("[{0}] Exception Handler捕获了一个异常:\r\n{1}", DateTime.Now.ToLocalTime().ToString(), exStack));
+                File.AppendAllText(Application.StartupPath + @"\Exception.txt", string.Format("\r\n====Exception====\r\nTime:{0}\r\n====Stack====\r\n{1}\r\n====Stack End====\r\n====Exception End====\r\n", DateTime.Now.ToLocalTime().ToString(), exStack));
+                Console.WriteLine(string.Format("[{0}] Exception Handler收到了一个异常:\r\n{1}", DateTime.Now.ToLocalTime().ToString(), exStack));
             }
             catch (Exception ex)
             {
