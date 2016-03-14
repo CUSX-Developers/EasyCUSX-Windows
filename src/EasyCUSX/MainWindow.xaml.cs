9af6763aca2acdff131a287070bcf6abc9ed8e96
StoneMoe <lakechan96@gmail.com>
2016-03-14 20:58:43 +0800

    Bump to 2.2.6

--- src/EasyCUSX/MainWindow.xaml.cs
+++ src/EasyCUSX/MainWindow.xaml.cs
@@ -27,7 +27,7 @@ namespace EasyCUSX
 
         string ProgramName = "易·山传";
         string ProgramTag = "easycusx_win";
-        string version = "2.2.5";
+        string version = "2.2.6";
 
         //Network
         bool WanConnecting = false;
