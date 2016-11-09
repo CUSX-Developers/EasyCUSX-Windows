5e664d8e6dd7fbd1d307fb4dcefe5798c884aa0f
StoneMoe <lakechan96@gmail.com>
2016-11-09 17:51:38 +0800

    Improve Domain-Level exception handler

--- src/EasyCUSX/App.xaml.cs
+++ src/EasyCUSX/App.xaml.cs
@@ -12,6 +12,7 @@ namespace EasyCUSX
     public partial class App : Application, ISingleInstanceApp
     {
         private const string Unique = "EasyCUSX2_Mutex_as456a8fc41as9d84af1f5f4f9g8s9f4"; //EasyCUSX2_Mutex_as456a8fc41as9d84af1f5f4f9g8s9f4
+        private const string errMsg = "易·山传发生了一个严重错误，程序将尝试忽略这个错误并继续工作，但有可能出现更多的未知问题，我们建议您立即重新启动易山传。\r\n如果该问题反复出现，请访问官方网站 https://cusx.net 下载安装包重新安装。\r\n如有更多疑问，可以反馈到易山传QQ群：512985336";
 
         [STAThread]
         public static void Main()
@@ -31,13 +32,13 @@ namespace EasyCUSX
 
         static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
         {
-            MessageBox.Show("易·山传发生了一个严重错误，程序将尝试忽略这个错误并继续工作，并有可能出现一些未知问题。\r\n如果该问题反复出现，请反馈至易山传QQ群：512985336");
+            MessageBox.Show(errMsg);
             new ExceptionHandler(e.ExceptionObject.ToString());
         }
 
         static void application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
         {
-            MessageBox.Show("易·山传发生了一个严重错误，程序将尝试忽略这个错误并继续工作，并有可能出现一些未知问题。\r\n如果该问题反复出现，请反馈至易山传QQ群：512985336");
+            MessageBox.Show(errMsg);
             new ExceptionHandler(e.Exception.ToString());
             e.Handled = true;
         }
